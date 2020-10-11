e8cbbd817d7e637353e5f7c8e051dc6bbc1ee480
Marcin Wierzbiński <marcin119a@gmail.com>
2020-10-11 22:45:49 +0200

    hash function for unordered map of paris

---
+++ include/ising/hash.hpp
@@ -0,0 +1,22 @@
+#ifndef HASH_NODE_HPP__
+#define HASH_NODE_HPP__
+
+#include <utility>
+#include <cstdint>
+#include <iostream>
+
+namespace ising
+{
+
+
+struct hash_pair {
+
+    template <class T1, class T2>
+    size_t operator()(const std::pair<T1, T2>& p) const {
+        auto lhs = std::hash<T1>{}(p.first); 
+        auto rhs = std::hash<T2>{}(p.second); 
+        return lhs^(rhs);
+    }
+};
+}
+#endif
--- include/ising/qubo.hpp
+++ include/ising/qubo.hpp
@@ -1,25 +1,27 @@
 #include <unordered_map>
 #include <ostream>
+#include <string>
+#include "ising/hash.hpp"
 
 #ifndef QUBO_MODEL_HPP__
 #define QUBO_MODEL_HPP__
 
 namespace qubo {
+
+                
         template <class NodeType, class CoefType>
         using LinearCoef = std::unordered_map<NodeType, CoefType>; 
 
         template <class NodeType, class CoefType>
-        using QuadraticCoef = std::unordered_map<std::pair<NodeType, NodeType>, CoefType>; 
-        
+        using QuadraticCoef = std::unordered_map<std::pair<NodeType, NodeType>, CoefType, ising::hash_pair>;
+
 
         template<class NodeType, class CoefType>
         class QUBOModel
         {
             protected: 
-
                 LinearCoef<NodeType, CoefType> linear;
                 QuadraticCoef<NodeType, CoefType> quadratic; 
-
 
 
             public:
